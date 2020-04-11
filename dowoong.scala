11df0ec4506437960f832f8f71883ffab149d34c
pyun19 <62095363+pyun19@users.noreply.github.com>
2020-04-11 22:33:34 +0900

    Update dowoong.scala

--- dowoong.scala
+++ dowoong.scala
@@ -5,10 +5,10 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.SparkException
 //////////////////////
-object rnd_result {
+//object rnd_result {
 
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder.master("local[*]").appName("Random").getOrCreate()
+//  def main(args: Array[String]): Unit = {
+//    val spark = SparkSession.builder.master("local[*]").appName("Random").getOrCreate()
 ///////////////////////
     val sc = spark.sparkContext
 
@@ -47,4 +47,45 @@ object rnd_result {
     val myDF3 = list1.toDF().orderBy(desc("Result")).limit(10)
     myDF3.show()
   }
+//}
+
+----------------------------------------------------협업필터링
+
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
+
+case class Rating(id: Int, SIM_Result: Double, TRUST_Result: Double, Result: Double)
+def parseRating(str: String): Rating = {
+  val fields = str.split("::")
+  assert(fields.size == 4)
+  Rating(fields(0).toInt, fields(1).toDouble, fields(2).toDouble, fields(3).toDouble)
 }
+
+val ratings = spark.read.myClass2.map(parseRating).toDF()
+val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
+
+// Build the recommendation model using ALS on the training data
+val als = new ALS().setMaxIter(5).setRegParam(0.01).setUserCol("id").setItemCol("SIM_Result").setItemCol("TRUST_Result").setRatingCol("Result")
+val model = als.fit(training)
+
+// Evaluate the model by computing the RMSE on the test data
+// Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
+model.setColdStartStrategy("drop")
+val predictions = model.transform(test)
+
+val evaluator = new RegressionEvaluator().setMetricName("rmse").setLabelCol("rating").setPredictionCol("prediction")
+
+val rmse = evaluator.evaluate(predictions)
+println(s"Root-mean-square error = $rmse")
+
+// Generate top 10 movie recommendations for each user
+val userRecs = model.recommendForAllUsers(10)
+// Generate top 10 user recommendations for each movie
+val movieRecs = model.recommendForAllItems(10)
+
+// Generate top 10 movie recommendations for a specified set of users
+val users = ratings.select(als.getUserCol).distinct().limit(3)
+val userSubsetRecs = model.recommendForUserSubset(users, 10)
+// Generate top 10 user recommendations for a specified set of movies
+val movies = ratings.select(als.getItemCol).distinct().limit(3)
+val movieSubSetRecs = model.recommendForItemSubset(movies, 10)
