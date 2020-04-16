d8298bafb0f2628cb8ed297579263f037fd17d52
smlee-git <ppsoming@gamil.com>
2020-04-16 15:21:37 +0900

    Update 3.test_lhb.scala

--- 3.test_lhb.scala
+++ 3.test_lhb.scala
@@ -37,7 +37,9 @@ var sbjtNM_in_departNM = clPassUri_DF.filter(clPassUri_DF("SUST_CD_NM").equalTo(
 sbjtNM_in_departNM.show
 
 //@@@ 컴퓨터공학과에서 개설된 수업명을 리스트로 생성
-var sbjtNM_List = sbjtNM_in_departNM.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList
+// var sbjtNM_List = sbjtNM_in_departNM.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList
+// 과목명이 1452개 -> distinct 지정distinct하게 자름 => 108개
+var sbjtNM_List = sbjtNM_in_departNM.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList.distinct
 
 //컴퓨터공학과 학생 학번을 가지고 그 사람이 수강한 교과목 리스트 생성
 //학과로 filter
@@ -46,109 +48,126 @@ var sbjtNM_List = sbjtNM_in_departNM.select("SBJT_KOR_NM").rdd.map(r=>r(0)).coll
 
 var departNM = "컴퓨터공학과"
 var std_NO = 20190030
-var student_have_sbjt_temp1 = clPassUri_DF.filter(clPassUri_DF("SUST_CD_NM").equalTo(s"${departNM}"))
-var student_have_sbjt_temp2 = student_have_sbjt_temp1.filter(student_have_sbjt_temp1("STD_NO").equalTo(s"${std_NO}"))
-var student_have_sbjt_temp3 = student_have_sbjt_temp2.select(col("SBJT_KOR_NM"))
+// 20142820 다른 학생
+//
+// //-----------------------------------------------학생 한명----------------------------------------------------
+// var student_have_sbjt_temp1 = clPassUri_DF.filter(clPassUri_DF("SUST_CD_NM").equalTo(s"${departNM}"))
+// var student_have_sbjt_temp2 = student_have_sbjt_temp1.filter(student_have_sbjt_temp1("STD_NO").equalTo(s"${std_NO}"))
+// var student_have_sbjt_temp3 = student_have_sbjt_temp2.select(col("SBJT_KOR_NM"))
+//
+// //@@@ 컴퓨터공학과의 학생 한명이 수강한 수업 리스트를 생성
+// var student_have_sbjt_List = student_have_sbjt_temp3.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList.distinct
+//
+// val isListened_List_temp1 = sbjtNM_List.map(x => (x, 0)).map{ record =>
+//   //x : record_1
+//   //0 : record_2
+//   //isListend면 1로 바뀜
+//   val name = record._1
+//   val isListened =
+//     if(student_have_sbjt_List.contains(name)) {
+//       1
+//     }
+//     else 0
+//   val isListened_List_temp2 = (name, isListened)
+//   print(isListened_List_temp2)
+//   //리턴하려면 이름을 쳐야 함
+//   //최종적으로 isListened_List_temp1 = isListened_List_temp2 값이 담기는 것 !!
+//   isListened_List_temp2
+// }
+// val isListened_List = isListened_List_temp1.map(_._2)
+// //-----------------------------------------------=----------------------------------------------------------------
 
-//@@@ 컴퓨터공학과의 학생 한명이 수강한 수업 리스트를 생성
-var student_have_sbjt_List = student_have_sbjt_temp3.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList.distinct
 
-val isListened_List_temp1 = sbjtNM_List.map(x => (x, 0)).map{ record =>
-  //x : record_1
-  //0 : record_2
-  //isListend면 1로 바뀜
-  val name = record._1
-  val isListened =
-    if(student_have_sbjt_List.contains(name)) {
-      1
-    }
-    else 0
-  val isListened_List_temp2 = (name, isListened)
-  print(isListened_List_temp2)
-  //리턴하려면 이름을 쳐야 함
-  //최종적으로 isListened_List_temp1 = isListened_List_temp2 값이 담기는 것 !!
-  isListened_List_temp2
-}
-val isListened_List = isListened_List_temp1.map(_._2)
+//==================================================일단 계산ㅇ ㅣ너무 느리니까 다른 방법으로,,=======================================
+var tuples = Seq[(Int, List[Int])]()
 
+// 학번이 1452개 -> distinct 지정 -> 223 명
+var stdNO_in_departNM = clPassUri_DF.filter(clPassUri_DF("SUST_CD_NM").equalTo(s"${departNM}")).select(col("STD_NO")).rdd.map(r=>r(0)).collect.toList.distinct
 
+stdNO_in_departNM.foreach{ stdNO =>
 
-students_in_departNM.foreach{ stdNO =>
-  println(stdNO)
-  var depart_activity_temp = List[Any]()
-  var depart_code_list = List[Any]("OAMTYPCD03", "OAMTYPCD04", "OAMTYPCD05")
+  // 학생별로 (stdNO) 들은 교과목 테이블
+  var student_have_sbjt_temp1 = sbjtNM_in_departNM.filter(sbjtNM_in_departNM("STD_NO").equalTo(s"${stdNO}"))
+  var student_have_sbjt_temp2 = student_have_sbjt_temp1.select(col("SBJT_KOR_NM"))
+  student_have_sbjt_temp2.show
 
-  //List1 : 코드(중복제거x) -> map 함수로 df에서 list 변환
-  //List2 : 이름(중복제거) -> map 함수로 df에서 list 변환
-  //List3 : List1 + List2 = 코드리스트 + 이름리스트 (학생 한명이 수행한 자율활동내용)
-  //---------------------자율활동 code list(자격증01, 어학02)----------------------
-  //5개의 코드 (학생 한명이 수행한 봉사03, 대외04, 기관05을 코드 별로 groupby count list)
-  var outAct_code_temp1 = outActUri_DF.filter(outActUri_DF("OAM_STD_NO").equalTo(s"${stdNO}")).select(col("OAM_STD_NO"),col("OAM_TYPE_CD"), col("OAM_TITLE"))
-  //3개의 코드만 필터링
-  var outAct_code_temp2 = outAct_code_temp1.filter($"OAM_TYPE_CD" === "OAMTYPCD03" || $"OAM_TYPE_CD" ==="OAMTYPCD04" || $"OAM_TYPE_CD" ==="OAMTYPCD05")
-  //학생 한명의 활동 코드만 존재하는 dataframe
-  var outAct_code_temp3 = outAct_code_temp2.drop("OAM_STD_NO", "OAM_TITLE").groupBy("OAM_TYPE_CD").count()
-
-  //모든 학과 모든 학생이 수행한 자율활동은 총 845개인데
-  //광홍과 201937039학생이 수행한 활동만 260개이고 나머지 광홍과 학생들의 데이터는 존재하지 않음
-
-  //이제 코드 3개에 대해 groupby와 agg 연산을 사용하여 코드 별로 count
-  var outAct_code_List = outAct_code_temp3.select(col("count")).rdd.map(r=>r(0)).collect.toList
-  //------------------------------------------------------------------------------
-
-  //---------------------자율활동 name list(자격증01, 어학02)----------------------
-  //5개의 코드
-  var outAct_name_temp1 = outActUri_DF.filter(outActUri_DF("OAM_STD_NO").equalTo(s"${stdNO}")).select(col("OAM_STD_NO"), col("OAM_TITLE")).distinct
-  //3개의 코드만 필터링
-  var outAct_name_temp2 = outAct_name_temp1.drop("OAM_STD_NO", "OAM_TYPE_CD").filter($"OAM_TYPE_CD" === "OAMTYPCD01" || $"OAM_TYPE_CD" ==="OAMTYPCD02").distinct
-
-  var outAct_name_List = outAct_name_temp2.rdd.map(r=>r(0)).collect.toList
-  println(outAct_name_List)
-  // var t_size = outAct_name_List.length
-  // if(t_size > 0) {
-    // println(s"##### SIZE : ${t_size} RESULT => " + outAct_name_List)
-  // }
-
-  depart_activity_temp = depart_activity_temp ++ outAct_name_List
-
-  //----------codeList + nameList = 학생 하나의 리스트-----------------------------
-  // var outAct_std_List = outAct_code_List ::: outAct_name_List
-  // outAct_std_List
-  //------------------------------------------------------------------------------
-
-  //학과 리스트
-  var depart_activity_List = depart_activity_temp.distinct
-  println(depart_activity_List)
-
-  //namelist로 유무 비교
-  //학생 name list 랑 학과 list를 비교해서 contain으로 1, 0
-
-  var activity_List_byStd_temp1 = depart_activity_List.map(x => (x, 0)).map{ activity =>
+  // 학과 전체 교과목 리스트를 순회 (교과, 0)으로 만들어놓음
+  val isListened_List_temp1 = sbjtNM_List.map(x => (x, 0)).map{ record =>
+    // println(s"stdNO : ${stdNO} ============= sbjtNM : ${record}")
+    //
+    //@@@ 컴퓨터공학과의 학생 한명이 수강한 수업 리스트를 생성
+    var student_have_sbjt_List = student_have_sbjt_temp2.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList.distinct
+    // println(s"student_have_sbjt_List--${student_have_sbjt_List}")
     //x : record_1
     //0 : record_2
     //isListend면 1로 바뀜
-    val actName = activity._1
-    val act =
-      if(outAct_name_List.contains(actName)) {
+    val name = record._1
+
+    val isListened =
+      if(student_have_sbjt_List.contains(name)) {
         1
       }
       else 0
-    var activity_List_byStd_temp2 = (actName, act)
-    print(activity_List_byStd_temp2)
+    val isListened_List_temp2 = (name, isListened)
+    // print(isListened_List_temp2)
     //리턴하려면 이름을 쳐야 함
     //최종적으로 isListened_List_temp1 = isListened_List_temp2 값이 담기는 것 !!
-    activity_List_byStd_temp2
+    isListened_List_temp2
   }
-  var activity_List_byStd_temp3 = activity_List_byStd_temp1.map(_._2)
-  println("activity_List_byStd:" + activity_List_byStd_temp3)
-  //학과 리스트를 돌면서 일치 여부 세는데
-  //봉사03, 대외04, 기관05 = 횟수 count
-  //자격증01, 어학02 = 유무(1 또는 0)
-
- activity_List_byStd = outAct_code_List ++ activity_List_byStd_temp3
- //학생 별 코드 횟수, 이름 유무 리스트 출력
- println("activity List !!! : " + activity_List_byStd)
+  val isListened_List = isListened_List_temp1.map(_._2).toString
+  println(isListened_List)
+  // tuples = tuples :+ (stdNO, isListened_List)
 }
+
+var df = tuples.toDF("STD_NO", "SUBJECT")
+
+//==========================================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var names = List[String]()
+var tuples = Seq[(Int, String)]()
+
+for(s<-0 until subcd_star_byStd_Map.size){
+  var star_point = List[Any]()
+  var order = List[Int]()
+  for(i<-0 until subcd_byDepart_List.size){
+    star_point = 0.0::star_point
+  }
+
+  for(i<-0 until subcd_star_byStd_Map(arr01(s).toString).size){
+    order = subcd_byDepart_List.indexOf(subcd_star_byStd_Map(arr01(s).toString)(i).subcd)::order
+    //names = subcd_star_byStd_Map(arr01(s).toString)(i).subcd::names
+  }
+
+  order = order.sorted
+
+  for(i<-0 until order.size){
+    var k=0;
+    //print(k)
+    while(subcd_byDepart_List(order(i))!=subcd_star_byStd_Map(arr01(s).toString)(k).subcd){
+    k+=1;
+    }
+
+    star_point = star_point.updated(order(i), subcd_star_byStd_Map(arr01(s).toString)(k).starpoint)
+  }
+  tuples = tuples :+ (arr01(s), star_point.toString)
+}
+
+var df = tuples.toDF("STD_NO", "RATING")
+
+
 
 
 
@@ -258,6 +277,7 @@ stdNO_in_departNM.foreach{ stdNO =>
   //자격증01, 어학02 = 유무(1 또는 0)
 
  activity_List_byStd = outAct_code_List ++ activity_List_byStd_temp3
+
  //학생 별 코드 횟수, 이름 유무 리스트 출력
  println("activity List !!! : " + activity_List_byStd)
 }
@@ -560,22 +580,34 @@ arr01.foreach{ stdNO =>
 // }
 
 
-
-var order = List[Int]()
-var star_point = List[Double]()
+//var star_point = List[Any]()
 var names = List[String]()
+var tuples = Seq[(Int, String)]()
 
-for(i<-0 until subcd_byDepart_List.size){
-  star_point = 0.0::star_point
+for(s<-0 until subcd_star_byStd_Map.size){
+  var star_point = List[Any]()
+  var order = List[Int]()
+  for(i<-0 until subcd_byDepart_List.size){
+    star_point = 0.0::star_point
+  }
+
+  for(i<-0 until subcd_star_byStd_Map(arr01(s).toString).size){
+    order = subcd_byDepart_List.indexOf(subcd_star_byStd_Map(arr01(s).toString)(i).subcd)::order
+    //names = subcd_star_byStd_Map(arr01(s).toString)(i).subcd::names
+  }
+
+  order = order.sorted
+
+  for(i<-0 until order.size){
+    var k=0;
+    //print(k)
+    while(subcd_byDepart_List(order(i))!=subcd_star_byStd_Map(arr01(s).toString)(k).subcd){
+    k+=1;
+    }
+
+    star_point = star_point.updated(order(i), subcd_star_byStd_Map(arr01(s).toString)(k).starpoint)
+  }
+  tuples = tuples :+ (arr01(s), star_point.toString)
 }
 
-for(i<-0 until subcd_star_byStd_Map(arr01(0).toString).size){
-  order = subcd_byDepart_List.indexOf(subcd_star_byStd_Map(arr01(0).toString)(i).subcd)::order
-  names = subcd_star_byStd_Map(arr01(0).toString)(i).subcd::names
-}
-
-order = order.sorted
-
-for(i<-0 until order.size){
-  subcd_star_byStd_Map(arr01(0).toString)()
-}
+var df = tuples.toDF("STD_NO", "RATING")
