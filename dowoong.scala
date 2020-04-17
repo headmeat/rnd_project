7963ff6d45a5e5e134bf68da6cf14a4ecdb9293b
pyun19 <62095363+pyun19@users.noreply.github.com>
2020-04-17 13:57:42 +0900

    도웅

--- dowoong.scala
+++ dowoong.scala
@@ -27,10 +27,6 @@ import org.apache.spark.SparkException
     //신뢰도 팀에서도 학번 별 신뢰도 dataframe을 줌
     //2개를 합쳐서 하나의 dataframe을 만들고 가중치를 주어 ranking값을 가진 새로운 dataframe을 생성
 
-
-
-
-
     val list0 = (1 to 100).map { x =>
       val tid = x
       val tSim = sim.nextDouble()
@@ -56,216 +52,69 @@ import org.apache.spark.SparkException
 
     val myDF3 = list1.toDF().orderBy(desc("Result")).limit(10)
     myDF3.show()
-  }
-//}
 
-----------------------------------------------------협업필터링
 
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.recommendation.ALS
+//////////////////////////////////////////////////////////////////////졸업생 N명 선정후 많이 들은 순으로 출력
+    /교과
 
-case class Rating(stdId: Int, pid: String, rating: Float, timestamp: Long)
-def parseRating(str: String): Rating = {
-  val fields = str.split("::")
-  assert(fields.size == 4)
-  Rating(fields(0).toInt, fields(1).toString, fields(2).toFloat, fields(3).toLong)
-}
+    var clPassUri_res = clPassUri_table.select(col("STD_NO"), col("SUST_CD_NM"), col("SBJT_KEY_CD"), col("SBJT_KOR_NM")).distinct.toDF
+    clPassUri_res.show()
 
+    val std_arr = Seq(20190030, 20170063, 20142915, 20152634, 20142824)
+    val std_sbjt_arr = std_arr.map{ stdno =>
+    val res = clPassUri_res.filter(clPassUri_res("STD_NO").equalTo(s"${stdno}"))
+    res
+    }.map{ x =>
+    val res = x.select("SBJT_KOR_NM").collect.toList.map( x=> x.toString).distinct
+    res
+    }.flatMap( x=> x).groupBy(x => x).mapValues(_.length).toList.sortBy(x => x._2).reverse
 
+    val top5 = std_sbjt_arr.take(5)
 
-//DB에 저장된 교과/비교과/자율활동 리스트를 dataframe 형태로 변환
-//1. DB에서 값 불러오기
-//학생 하나에 대해 교과, 비교과, 자율활동 list가 존재
-//학생 하나의 dataframe을 불러오면
 
 
-//2. list를 dataframe으로 변환
 
 
-//3. 교과, 비교과, 자율 각각 ratings를 생성 (컴공과 하나에 대한 document를 가져와서 dataframe으로 변환)
-val ratings
 
+    /비교과
 
+    // 데이터 학번 20142820, 20142932, 20152611, 20152615
 
-val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
+    var cpsStarUri_res = cpsStarUri_table.select(col("STD_NO"), col("STAR_KEY_ID"), col("YEAR"),col("SMESTR")).distinct.toDF
+        cpsStarUri_res.show()
 
-// Build the recommendation model using ALS on the training data
-val als = new ALS().setMaxIter(5).setRegParam(0.01).setUserCol("id").setItemCol("SIM_Result").setItemCol("TRUST_Result").setRatingCol("Result")
-val model = als.fit(training)
 
-// Evaluate the model by computing the RMSE on the test data
-// Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
-model.setColdStartStrategy("drop")
-val predictions = model.transform(test)
+//  val dfs_2 = Seq(std_STAR_KEY_list1, std_STAR_KEY_list2, std_STAR_KEY_list3, std_STAR_KEY_list4)
+    val std_arr2 = Seq(20142820, 20142932, 20152611, 20152615)
+    val std_STAR_KEY_arr2 = std_arr2.map{ stdno =>
+    val res = cpsStarUri_res.filter(cpsStarUri_res("STD_NO").equalTo(s"${stdno}"))
+        res
+        }.map{ x =>
+    val res = x.select("STAR_KEY_ID").collect.toList.map( x=> x.toString)
+        res
+        }.flatMap( x=> x).groupBy(x => x).mapValues(_.length).toList.sortBy(x => x._2).reverse
 
-val evaluator = new RegressionEvaluator().setMetricName("rmse").setLabelCol("rating").setPredictionCol("prediction")
+    val top5 = std_STAR_KEY_arr2.take(5)
 
-val rmse = evaluator.evaluate(predictions)
-println(s"Root-mean-square error = $rmse")
 
-// Generate top 10 movie recommendations for each user
-val userRecs = model.recommendForAllUsers(10)
-// Generate top 10 user recommendations for each movie
-val movieRecs = model.recommendForAllItems(10)
 
-// Generate top 10 movie recommendations for a specified set of users
-val users = ratings.select(als.getUserCol).distinct().limit(3)
-val userSubsetRecs = model.recommendForUserSubset(users, 10)
-// Generate top 10 user recommendations for a specified set of movies
-val movies = ratings.select(als.getItemCol).distinct().limit(3)
-val movieSubSetRecs = model.recommendForItemSubset(movies, 10)
-================================================================================================================
 
+    /자율 활동
 
+    var clPassUri_res = clPassUri_table.select(col("STD_NO"), col("SUST_CD_NM"), col("SBJT_KEY_CD"), col("SBJT_KOR_NM")).distinct.toDF
+    clPassUri_res.show()
 
-package com.packt.ScalaML.MovieRecommendation
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.mllib.recommendation.ALS
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
-import org.apache.spark.mllib.recommendation.Rating
-import scala.Tuple2
-import org.apache.spark.rdd.RDD
+    val std_arr = (20190030, 20170063, 20142915, 20152634, 20142824)
 
 
-val ratigsFile = "data/ratings.csv"
-val df1 = spark.read.format("com.databricks.spark.csv").option("header", true).load(ratigsFile)
-val ratingsDF = df1.select(df1.col("userId"), df1.col("movieId"), df1.col("rating"), df1.col("timestamp"))
-ratingsDF.show(false)
+    val dfs = Seq(std_sbjt_list1, std_sbjt_list2, std_sbjt_list3, std_sbjt_list4, std_sbjt_list5)
+    val std_arr = Seq(20190030, 20170063, 20142915, 20152634, 20142824)
+    val std_sbjt_arr = std_arr.map{ stdno =>
+    val res = clPassUri_res.filter(clPassUri_res("STD_NO").equalTo(s"${stdno}"))
+    res
+    }.map{ x =>
+    val res = x.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList.map( x=> x.toString).distinct
+    res
+    }.flatMap( x=> x).groupBy(x => x).mapValues(_.length).toList.sortBy(x => x._2).reverse
 
-
-val moviesFile = "data/movies.csv"
-val df2 = spark.read.format("com.databricks.spark.csv").option("header", "true").load(moviesFile)
-val moviesDF = df2.select(df2.col("movieId"), df2.col("title"), df2.col("genres"))
-
-
-ratingsDF.createOrReplaceTempView("ratings")
-moviesDF.createOrReplaceTempView("movies")
-
-
-val numRatings = ratingsDF.count()
-val numUsers = ratingsDF.select(ratingsDF.col("userId")).distinct().count()
-val numMovies = ratingsDF.select(ratingsDF.col("movieId")).distinct().count() println("Got " + numRatings + " ratings from " + numUsers + " users on " + numMovies + " movies.")
-
-
-val results = spark.sql("select movies.title, movierates.maxr, movierates.minr, movierates.cntu "
-+ "from(SELECT ratings.movieId,max(ratings.rating) as maxr,"
-+ "min(ratings.rating) as minr,count(distinct userId) as cntu "
-+ "FROM ratings group by ratings.movieId) movierates "
-+ "join movies on movierates.movieId=movies.movieId "
-+ "order by movierates.cntu desc") results.show(false)
-
-val mostActiveUsersSchemaRDD = spark.sql("SELECT ratings.userId, count(*) as ct from ratings "+ "group by ratings.userId order by ct desc limit 10")mostActiveUsersSchemaRDD.show(false)
-
-
-val results2 = spark.sql(
-"SELECT ratings.userId, ratings.movieId,"
-+ "ratings.rating, movies.title FROM ratings JOIN movies"
-+ "ON movies.movieId=ratings.movieId"
-+ "where ratings.userId=668 and ratings.rating > 4") results2.show(false)
-
-
-val splits = ratingsDF.randomSplit(Array(0.75, 0.25), seed = 12345L)
-val (trainingData, testData) = (splits(0), splits(1))
-val numTraining = trainingData.count()
-val numTest = testData.count()
-println("Training: " + numTraining + " test: " + numTest)
-
-val ratingsRDD = trainingData.rdd.map(row => {
-val userId = row.getString(0)
-val movieId = row.getString(1)
-val ratings = row.getString(2)
-Rating(userId.toInt, movieId.toInt, ratings.toDouble)
-})
-
-val testRDD = testData.rdd.map(row => {
-val userId = row.getString(0)
-val movieId = row.getString(1)
-val ratings = row.getString(2)
-Rating(userId.toInt, movieId.toInt, ratings.toDouble)
-})
-
-
-val rank = 20
-val numIterations = 15
-val lambda = 0.10
-val alpha = 1.00 val block = -1
-val seed = 12345L
-val implicitPrefs = false
-val model = new ALS().setIterations(numIterations) .setBlocks(block).setAlpha(alpha)
-.setLambda(lambda)
-.setRank(rank) .setSeed(seed)
-.setImplicitPrefs(implicitPrefs)
-.run(ratingsRDD)
-
-
-println("Rating:(UserID, MovieID, Rating)")
-println("----------------------------------")
-val topRecsForUser = model.recommendProducts(668, 6) for (rating <- topRecsForUser) { println(rating.toString()) } println("----------------------------------") >>>
-
-val rmseTest = computeRmse(model, testRDD, true)
-println("Test RMSE: = " + rmseTest) //Less is better
-
-def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating], implicitPrefs: Boolean): Double = { val predictions: RDD[Rating] = model.predict(data.map(x => (x.user, x.product))) valpredictionsAndRatings = predictions.map { x => ((x.user, x.product), x.rating) } .join(data.map(x => ((x.user, x.product), x.rating))).values if (implicitPrefs) { println("(Prediction, Rating)") println(predictionsAndRatings.take(5).mkString("n")) } math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).mean()) }println("Recommendations: (MovieId => Rating)") println("----------------------------------") val recommendationsUser = model.recommendProducts(668, 6) recommendationsUser.map(rating => (rating.product, rating.rating)).foreach(println) println("----------------------------------")
-
-
-println("Recommendations: (MovieId => Rating)") println("----------------------------------") val recommendationsUser = model.recommendProducts(668, 6) recommendationsUser.map(rating => (rating.product, rating.rating)).foreach(println) println("----------------------------------")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-------------------------------------------------------------------------------------------------------0413
-
-
-//컴퓨터공학과 학생 학번을 가지고 그 사람이 수강한 교과목 리스트 생성
-//학과로 filter
-//학번으로 filter해서
-//수업명을 리스트로 생성
-
-
-var departNM = "컴퓨터공학과"
-var std_NO = 20190030
-var student_have_sbjt_temp1 = clPassUri_DF.filter(clPassUri_DF("SUST_CD_NM").equalTo(s"${departNM}"))
-var student_have_sbjt_temp2 = student_have_sbjt_temp1.filter(student_have_sbjt_temp1("STD_NO").equalTo(s"${std_NO}"))
-var student_have_sbjt_temp3 = student_have_sbjt_temp2.select(col("SBJT_KOR_NM"))
-
-//@@@ 컴퓨터공학과의 학생 한명이 수강한 수업 리스트를 생성
-var student_have_sbjt_List = student_have_sbjt_temp3.select("SBJT_KOR_NM").rdd.map(r=>r(0)).collect.toList.distinct
-
-
-val isListened_List_temp1 = sbjtNM_List.map(x => (x, 0)).map{ record =>
-  //x : record_1
-  //0 : record_2
-  //isListend면 1로 바뀜
-  val name = record._1
-  val isListened =
-    if(student_have_sbjt_List.contains(name)) {
-      1
-    }
-    else 0
-  val isListened_List_temp2 = (name, isListened)
-  print(isListened_List_temp2)
-  //리턴하려면 이름을 쳐야 함
-  //최종적으로 isListened_List_temp1 = isListened_List_temp2 값이 담기는 것 !!
-  isListened_List_temp2
-}
-val isListened_List = isListened_List_temp1.map(_._2)
+    val top5 = std_sbjt_arr.take(5)
