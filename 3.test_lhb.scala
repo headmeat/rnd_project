226c551802b7f08e27ce7642114a2524aa897c5c
netdb <byh741@naver.com>
2020-04-23 13:56:28 +0900

    u

--- 3.test_lhb.scala
+++ 3.test_lhb.scala
@@ -1,6 +1,21 @@
-package kr.ac.cbnu ~~~~
+package kr.ac.cbnu.RnD
 
-object TABLE_INFO {
+object TABLE_COLLUM_INFO {
+  //교과
   val STD_NO = "STD_NO"
+  val SUST_CD_NM = "SUST_CD_NM"
+  val SBJT_KOR_NM = "SBJT_KOR_NM"
+  val SBJT_KEY_CD = "SBJT_KEY_CD"
+  val STAR_KEY_ID = "STAR_KEY_ID"
+  val STAR_POINT = "STAR_POINT"
+  val TYPE = "TYPE"
 
+  //비교과
+  val NPI_KEY_ID = "NPI_KEY_ID"
+  val NPI_AREA_SUB_CD = "NPI_AREA_SUB_CD"
+
+  //자율활동
+  val OAM_STD_NO = "OAM_STD_NO"
+  val OAM_TYPE_CD = "OAM_TYPE_CD"
+  val OAM_TITLE = "OAM_TITLE"
 }
