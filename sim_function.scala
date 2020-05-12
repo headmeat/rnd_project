2b2ea5c9bddcb09319d6e877f7eae4188365dc59
smlee-git <ppsoming@gamil.com>
2020-05-12 23:52:38 +0900

    Update sim_function.scala

--- sim_function.scala
+++ sim_function.scala
@@ -446,7 +446,13 @@ stdNO_in_departNM_act.foreach{ stdNO =>
       if(outAct_name_List.contains(actName)) {
         1
       }
-      else 0
+      else -1
+      // if(outAct_name_List.contains(actName)) {
+      //   var act = 1
+      // }else{
+      //   var act = -1
+      // }
+
     var activity_List_byStd_temp2 = (actName, act)
     // println(activity_List_byStd_temp2)
     //리턴하려면 이름을 쳐야 함
@@ -491,106 +497,139 @@ MongoSpark.save(
 
     // mongodb collection 제거
     // db.USER_SIMILARITY.drop()
-    var userforSimilarity_table = getMongoDF(spark, userforSimilarityUri) //유사도 분석 팀이 생성한 테이블
+    //var userforSimilarity_table = getMongoDF(spark, userforSimilarityUri) //유사도 분석 팀이 생성한 테이블
+
+    val schema_sim = StructType(
+        StructField("STD_NO", StringType, true) ::
+        StructField("SUBJECT_STAR", StringType, true) ::
+        StructField("NCR_STAR", StringType, true) ::
+        StructField("ACTING_COUNT", StringType, true) :: Nil)
+    var userforSimilarity_table = spark.createDataFrame(sc.emptyRDD[Row], schema_sim)
+
+    //
+    // var userforSimilarity_table = spark.createDataFrame(sc.emptyRDD[Row], schema_rdd)
+    // try{
+    //   userforSimilarity_table = getMongoDF(spark, userforSimilarityUri) //유사도 분석 팀이 생성한 테이블
+    // }
+    // catch{
+    //   case e: NoSuchElementException => println("ERROR!")
+    //   val empty_df = sqlContext.createDataFrame(sc.emptyRDD[Row], schema_rdd)
+    //   empty_df
+    // }
 
     var userforSimilarity_df = userforSimilarity_table.select(col("STD_NO"), col("SUBJECT_STAR"), col("NCR_STAR"), col("ACTING_COUNT"))
     userforSimilarity_df = userforSimilarity_df.drop("_id")
 
-    //질의자
-    // var querySTD_NO = 20142820
-    var querySTD = spark.createDataFrame(sc.emptyRDD[Row], schema_rdd)
-
-    try{
-      querySTD = userforSimilarity_df.filter(userforSimilarity_df("STD_NO").equalTo(s"${std_NO}")).drop("STD_NO")
-    }
-    catch{
-      case e: NoSuchElementException => println("ERROR!")
-      val empty_df = sqlContext.createDataFrame(sc.emptyRDD[Row], schema_rdd)
-      empty_df
-    }
-
-    val exStr = "WrappedArray|\\(|\\)|\\]|\\["
-
-    var querySTD_List = querySTD.collect.toList.mkString.replaceAll(exStr, "").split(",").map(x => (x.trim.toDouble * 10).toInt)
-    var sbjt_star = querySTD.select(col("SUBJECT_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
-    var ncr_star = querySTD.select(col("NCR_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
-    var acting_count = querySTD.select(col("ACTING_COUNT")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
-
-    var w1, w2, w3 = 0.33 //가중치 값
-    var a, b, r = 1 //알파, 베타, 감마
-
-    object CosineSimilarity {
-       def dotProduct(x: Array[Int], y: Array[Int]): Int = {
-         (for((a, b) <- x zip y) yield a * b) sum
-       }
-       def magnitude(x: Array[Int]): Double = {
-         math.sqrt(x map(i => i*i) sum)
-       }
-      def cosineSimilarity(x: Array[Int], y: Array[Int]): Double = {
-        require(x.size == y.size)
-        dotProduct(x, y)/(magnitude(x) * magnitude(y))
+    if(userforSimilarity_df.count != 0){
+      //질의자
+      // var querySTD_NO = 20142820
+      // var querySTD = userforSimilarity_df.filter(userforSimilarity_df("STD_NO").equalTo(s"${std_NO}")).drop("STD_NO")
+      var querySTD = spark.createDataFrame(sc.emptyRDD[Row], schema_rdd)
+      try{
+        querySTD = userforSimilarity_df.filter(userforSimilarity_df("STD_NO").equalTo(s"${std_NO}")).drop("STD_NO")
       }
+      catch{
+        case e: NoSuchElementException => println("ERROR!")
+        val empty_df = sqlContext.createDataFrame(sc.emptyRDD[Row], schema_rdd)
+        empty_df
+      }
+
+      val exStr = "WrappedArray|\\(|\\)|\\]|\\["
+
+      var querySTD_List = querySTD.collect.toList.mkString.replaceAll(exStr, "").split(",").map(x => (x.trim.toDouble * 10).toInt)
+      var sbjt_star = querySTD.select(col("SUBJECT_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
+      var ncr_star = querySTD.select(col("NCR_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
+      var acting_count = querySTD.select(col("ACTING_COUNT")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10 * -10).toInt)
+
+      var w1, w2, w3 = 0.33 //가중치 값
+      var a, b, r = 1 //알파, 베타, 감마
+
+      object CosineSimilarity {
+         def dotProduct(x: Array[Int], y: Array[Int]): Int = {
+           (for((a, b) <- x zip y) yield a * b) sum
+         }
+         def magnitude(x: Array[Int]): Double = {
+           math.sqrt(x map(i => i*i) sum)
+         }
+        def cosineSimilarity(x: Array[Int], y: Array[Int]): Double = {
+          require(x.size == y.size)
+          dotProduct(x, y)/(magnitude(x) * magnitude(y))
+        }
+      }
+
+      var user_sim_tuples = Seq[(String, Double)]()
+      var user_sim_tuples_sbjt = Seq[(String, Double)]()
+      var user_sim_tuples_ncr = Seq[(String, Double)]()
+      var user_sim_tuples_act = Seq[(String, Double)]()
+      var stdNO_inDepart_List = userforSimilarity_df.select(col("STD_NO")).rdd.map(x => x(0)).collect().toList
+
+      stdNO_inDepart_List.foreach(stdNO => {
+        // var i = 0; //토탈 구해줄 떄 바꿀라고
+        // println(stdNO)
+        //유사사용자
+        var std_inDepart = userforSimilarity_df.filter(userforSimilarity_df("STD_NO").equalTo(s"${stdNO}")).drop("STD_NO")
+        var std_inDepart_List = std_inDepart.collect.toList.mkString.replaceAll(exStr, "").split(",").map(x => (x.trim.toDouble * 10).toInt)
+
+        //교과
+        var sbjt_star_ = std_inDepart.select(col("SUBJECT_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
+        //비교과
+        var ncr_star_ = std_inDepart.select(col("NCR_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
+        //자율활동
+        var acting_count_ = std_inDepart.select(col("ACTING_COUNT")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10 * -10).toInt)
+        // println(acting_count_)
+        val sim = CosineSimilarity.cosineSimilarity(querySTD_List, std_inDepart_List)
+        val sbjt_sim = CosineSimilarity.cosineSimilarity(sbjt_star, sbjt_star_)
+        // println("sbjt_sim  : " + sbjt_sim)
+        val ncr_sim = CosineSimilarity.cosineSimilarity(ncr_star, ncr_star_)
+        // println("ncr_sim  : " + ncr_sim)
+        val acting_sim = CosineSimilarity.cosineSimilarity(acting_count, acting_count_)
+        // println("acting_sim  : " + acting_sim)
+        //println(sim)
+
+        user_sim_tuples_sbjt = user_sim_tuples_sbjt :+ (s"${stdNO}", sbjt_sim)
+        user_sim_tuples_ncr = user_sim_tuples_ncr :+ (s"${stdNO}", ncr_sim)
+        user_sim_tuples_act = user_sim_tuples_act :+ (s"${stdNO}", acting_sim)
+
+        // var total_sim = (user_sim_tuples_sbjt(i)._2 * w1) + (user_sim_tuples_ncr(i)._2 * w2) + (user_sim_tuples_act(i)._2 * w3)
+        var total_sim = (sbjt_sim * w1) + (ncr_sim * w2) + (acting_sim * w3)
+        // println(total_sim)
+        //
+        // i += 1;
+
+        //전체 유사도 구하기
+        user_sim_tuples = user_sim_tuples :+ (s"${stdNO}", total_sim)
+      })
+
+      var user_sim_df = user_sim_tuples.toDF("STD_NO", "similarity")
+      var user_sim_sbjt_df = user_sim_tuples_sbjt.toDF("STD_NO", "sbjt_similarity")
+      var user_sim_ncr_df = user_sim_tuples_ncr.toDF("STD_NO", "ncr_similarity")
+      var user_sim_acting_df = user_sim_tuples_act.toDF("STD_NO", "acting_similarity")
+
+      var join_df_temp1 = user_sim_sbjt_df.join(user_sim_ncr_df, Seq("STD_NO"), "outer")
+      var join_df_temp2 = join_df_temp1.join(user_sim_acting_df, Seq("STD_NO"), "outer")
+      val user_sim_join_df = join_df_temp2.join(user_sim_df, Seq("STD_NO"), "outer")
+
+      MongoSpark.save(
+      user_sim_join_df.write
+          .option("spark.mongodb.output.uri", "mongodb://127.0.0.1/cpmongo_distinct.USER_SIMILARITY")
+          .mode("overwrite")
+        )
+      user_sim_join_df
+    }else{
+      val schema_totalsim = StructType(
+          StructField("STD_NO", StringType, true) ::
+          StructField("sbjt_similarity", StringType, true) ::
+          StructField("ncr_similarity", StringType, true) ::
+          StructField("acting_similarity", StringType, true) ::
+          StructField("similarity", StringType, true) :: Nil)
+      var user_sim_join_df = spark.createDataFrame(sc.emptyRDD[Row], schema_totalsim)
+      MongoSpark.save(
+      user_sim_join_df.write
+          .option("spark.mongodb.output.uri", "mongodb://127.0.0.1/cpmongo_distinct.USER_SIMILARITY")
+          .mode("overwrite")
+        )
+      user_sim_join_df
     }
-
-    var user_sim_tuples = Seq[(String, Double)]()
-    var user_sim_tuples_sbjt = Seq[(String, Double)]()
-    var user_sim_tuples_ncr = Seq[(String, Double)]()
-    var user_sim_tuples_act = Seq[(String, Double)]()
-    var stdNO_inDepart_List = userforSimilarity_df.select(col("STD_NO")).rdd.map(x => x(0)).collect().toList
-
-    stdNO_inDepart_List.foreach(stdNO => {
-      // var i = 0; //토탈 구해줄 떄 바꿀라고
-
-      println(stdNO)
-      //유사사용자
-      var std_inDepart = userforSimilarity_df.filter(userforSimilarity_df("STD_NO").equalTo(s"${stdNO}")).drop("STD_NO")
-      var std_inDepart_List = std_inDepart.collect.toList.mkString.replaceAll(exStr, "").split(",").map(x => (x.trim.toDouble * 10).toInt)
-
-      //교과
-      var sbjt_star_ = std_inDepart.select(col("SUBJECT_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
-      //비교과
-      var ncr_star_ = std_inDepart.select(col("NCR_STAR")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
-      //자율활동
-      var acting_count_ = std_inDepart.select(col("ACTING_COUNT")).collect.toList.mkString.replaceAll(exStr, "").split(",").map(x=>(x.trim.toDouble * 10).toInt)
-
-      //val sim = CosineSimilarity.cosineSimilarity(querySTD_List, std_inDepart_List)
-      val sbjt_sim = CosineSimilarity.cosineSimilarity(sbjt_star, sbjt_star_)
-      val ncr_sim = CosineSimilarity.cosineSimilarity(ncr_star, ncr_star_)
-      val acting_sim = CosineSimilarity.cosineSimilarity(acting_count, acting_count_)
-
-      //println(sim)
-      //user_sim_tuples = user_sim_tuples :+ (s"${stdNO}", sim)
-      user_sim_tuples_sbjt = user_sim_tuples_sbjt :+ (s"${stdNO}", sbjt_sim)
-      user_sim_tuples_ncr = user_sim_tuples_ncr :+ (s"${stdNO}", ncr_sim)
-      user_sim_tuples_act = user_sim_tuples_act :+ (s"${stdNO}", acting_sim)
-
-      // var total_sim = (user_sim_tuples_sbjt(i)._2 * w1) + (user_sim_tuples_ncr(i)._2 * w2) + (user_sim_tuples_act(i)._2 * w3)
-      var total_sim = (sbjt_sim * w1) + (ncr_sim * w2) + (acting_sim * w3)
-      println(total_sim)
-      //
-      // i += 1;
-
-      //전체 유사도 구하기
-      user_sim_tuples = user_sim_tuples :+ (s"${stdNO}", total_sim)
-
-    })
-
-    var user_sim_df = user_sim_tuples.toDF("STD_NO", "similarity")
-    var user_sim_sbjt_df = user_sim_tuples_sbjt.toDF("STD_NO", "sbjt_similarity")
-    var user_sim_ncr_df = user_sim_tuples_ncr.toDF("STD_NO", "ncr_similarity")
-    var user_sim_acting_df = user_sim_tuples_act.toDF("STD_NO", "acting_similarity")
-
-    var join_df_temp1 = user_sim_sbjt_df.join(user_sim_ncr_df, Seq("STD_NO"), "outer")
-    var join_df_temp2 = join_df_temp1.join(user_sim_acting_df, Seq("STD_NO"), "outer")
-    val user_sim_join_df = join_df_temp2.join(user_sim_df, Seq("STD_NO"), "outer")
-
     // setMongoDF_USER_SIM(spark, join_df)
-
-    MongoSpark.save(
-    user_sim_join_df.write
-        .option("spark.mongodb.output.uri", "mongodb://127.0.0.1/cpmongo_distinct.USER_SIMILARITY")
-        .mode("overwrite")
-      )
 //============================유사도(연희,소민) end=======================================
-user_sim_join_df
 }
